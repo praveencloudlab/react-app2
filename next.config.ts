3921cc54bd034fce62dbeabd107e6feb5075a5c4
praveencloudlab <richardsforu@gmail.com>
2026-03-19 17:34:40 +0000

    ACP-1 : Product ID export issue resoved

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,4 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: 'export',      // CRITICAL: This creates the 'out' folder
-  images: {
-    unoptimized: true,   // Required for static export
-  },
-};
-
-export default nextConfig;
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true }
+}
